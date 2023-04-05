5f91adaac3781d3fd304305588d7ef8e831be825
Nawroz Mohammadi <nawroz.mohammadi@cloudredux.com>
2023-04-05 12:28:40 +0530

    On scroll animation for gsapp using nuxt js

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [{ src: "~/plugins/gsap.js", mode: "client", ssr: false }],
+
+  gsap: {
+    /* Module Options */
+
+    extraPlugins: {
+      scrollTo: true,
+      scrollTrigger: true,
+    },
+
+    extraEases: {
+      expoScaleEase: true,
+    },
+  },
+};
